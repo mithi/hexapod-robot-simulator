1aa230e0517f66ea5e4d4667a6fd1213957d9586
Mithi <mithi.sevilla@gmail.com>
2020-02-16 21:23:36 +0800

    docs: Add ascii art

--- hexapod/const.py
+++ hexapod/const.py
@@ -1,6 +1,19 @@
 from .plotter import HexapodPlot
 from .models import VirtualHexapod
 
+#         x2          x1
+#          \         /
+#           *---*---*
+#          /    |    \
+#         /     |     \
+#        /      |      \
+#  x3 --*------cog------*-- x0
+#        \      |      /
+#         \     |     /
+#          \    |    /
+#           *---*---*
+#          /         \
+#         x4         x5
 NAMES_LEG = ['right-middle', 'right-front', 'left-front', 'left-middle', 'left-back', 'right-back']
 NAMES_JOINT = ['coxia', 'femur', 'tibia']
 
