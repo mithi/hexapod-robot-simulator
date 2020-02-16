e4b458425f239213f766d194e73fed3c3ff9b2b9
Mithi <mithi.sevilla@gmail.com>
2020-02-16 17:42:55 +0800

    Remove unused files

--- const.py
+++
@@ -1,9 +0,0 @@
-from hexaplot import HexapodPlot
-from hexapod import VirtualHexapod
-
-NAMES_LEG = ['right-middle', 'right-front', 'left-front', 'left-middle', 'left-back', 'right-back']
-NAMES_JOINT = ['coxia', 'femur', 'tibia']
-
-BASE_HEXAPOD = VirtualHexapod(100, 100, 100, 100, 100, 100)
-BASE_HEXAPLOT = HexapodPlot(BASE_HEXAPOD)
-
