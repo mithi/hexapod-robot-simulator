98a46bb012bd82df50848dbd74ead24b213fe551
Mithi <mithi.sevilla@gmail.com>
2020-02-16 16:14:13 +0800

    Change slider inputs to numeric  text inputs

--- apps/appmeasurements.py
+++ apps/appmeasurements.py
@@ -7,6 +7,7 @@ import plotly.graph_objs as go
 from hexapod import VirtualHexapod
 from hexaplot import HexapodPlot
 from sectioning import make_section_type3
+from measurementwidgets import INPUT_LENGTHS, SECTION_INPUT_LENGTHS, INPUT_LENGTHS_IDs
 
 from const import BASE_HEXAPLOT
 
@@ -23,26 +24,14 @@ SLIDER_ALPHA = dcc.Slider(id='slider-alpha', min=-90, max=90, marks=SLIDER_ANGLE
 SLIDER_BETA = dcc.Slider(id='slider-beta', min=-90, max=90, marks=SLIDER_ANGLE_MARKS, value=0, step=5)
 SLIDER_GAMMA = dcc.Slider(id='slider-gamma', min=-90, max=90, marks=SLIDER_ANGLE_MARKS, value=0, step=5)
 
-SLIDER_MARKS = {tick: str(tick) for tick in [20, 40, 60, 80, 100]}
-
-SLIDER_FRONT = dcc.Slider(id='slider-front', min=20, max=100, marks=SLIDER_MARKS, value=70, step=5)
-SLIDER_SIDE = dcc.Slider(id='slider-side', min=20, max=100, marks=SLIDER_MARKS, value=70, step=5)
-SLIDER_MIDDLE = dcc.Slider(id='slider-middle', min=20, max=100, marks=SLIDER_MARKS, value=70, step=5)
-
-SLIDER_COXIA = dcc.Slider(id='slider-coxia', min=20, max=100, marks=SLIDER_MARKS, value=70, step=5)
-SLIDER_FEMUR = dcc.Slider(id='slider-femur', min=20, max=100, marks=SLIDER_MARKS, value=70, step=5)
-SLIDER_TIBIA = dcc.Slider(id='slider-tibia', min=20, max=100, marks=SLIDER_MARKS, value=70, step=5)
-
 # -----------
 # SECTIONS AND LAYOUT
 # -----------
-section_sliders_body = make_section_type3(SLIDER_FRONT, SLIDER_SIDE, SLIDER_MIDDLE, 'Front Length', 'Side Length', 'Middle Length')
-section_sliders_leg = make_section_type3(SLIDER_COXIA, SLIDER_FEMUR, SLIDER_TIBIA, 'Coxia Length', 'Femur Length', 'Tibia Length')
 section_sliders_angles = make_section_type3(SLIDER_ALPHA, SLIDER_BETA, SLIDER_GAMMA, 'Alpha', 'Beta', 'Gamma')
 
 section_hexapod = html.Div([
   html.Div(dcc.Graph(id='hexapod-plot'), style={'width': '50%'}),
-  html.Div([section_sliders_body, section_sliders_leg, section_sliders_angles], style={'width': '40%'}),
+  html.Div([SECTION_INPUT_LENGTHS, section_sliders_angles], style={'width': '40%'}),
   html.Div(id='display-variables', style={'width': '10%'}),
   ], 
   style={'display': 'flex'})
@@ -56,20 +45,10 @@ layout = html.Div([
 # -----------
 # CALLBACKS
 # -----------
-SLIDER_IDs = [
-  'slider-alpha', 
-  'slider-beta', 
-  'slider-gamma',
-  'slider-front', 
-  'slider-side', 
-  'slider-middle',
-  'slider-coxia', 
-  'slider-femur', 
-  'slider-tibia',
-]
+INPUT_IDs = ['slider-alpha', 'slider-beta', 'slider-gamma'] + INPUT_LENGTHS_IDs
 @app.callback(
   Output('variables', 'children'),
-  [Input(i, 'value') for i in SLIDER_IDs]
+  [Input(i, 'value') for i in INPUT_IDs]
 )
 def update_variable(alpha, beta, gamma, f, s, m, h, k, a):
   return json.dumps({
@@ -98,7 +77,7 @@ def display_variables(pose_params):
 
 @app.callback(
   Output('hexapod-plot', 'figure'),
-  [Input(slider_id, 'value') for slider_id in SLIDER_IDs]
+  [Input(i, 'value') for i in INPUT_IDs]
 )
 def update_hexapod_plot(alpha, beta, gamma, f, s, m, h, k, a):
   virtual_hexapod = VirtualHexapod(h, k, a, f, m, s)
