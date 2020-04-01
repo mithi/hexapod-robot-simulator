82298af19bdb0c244d67639104d5ecd593c4935e
Mithi <mithi.sevilla@gmail.com>
2020-04-01 21:12:15 +0800

    Add preliminary ik page

--- pages/page_inverse.py
+++ pages/page_inverse.py
@@ -1,28 +1,46 @@
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-
+from widgets.ik_ui import IK_INPUT_IDs, SECTION_IK
 from app import app
-from app import HEXAPOD_MEASUREMENTS
+
+section_nothing = html.Label('Nothing')
+
+SECTION_LEFT = html.Div([
+    html.Div(SECTION_IK, style={'width': '60%'}),
+    html.Div(section_nothing, style={'width': '40%'}),
+  ],
+  style={'display': 'flex'}
+)
+
 
 layout = html.Div([
-  html.H3('Measurements'),
-  dcc.Dropdown(
-    id='measurement-dropdown',
-    options=[
-      {'label': i, 'value': i} for i in ['front_length', 'side_length', 'mid_length']
-    ]
-  ),
-  html.Div(id='measurement-display')
+  html.H1('Inverse Kinematics'),
+  SECTION_LEFT,
+  html.Div(id='ik-variables')
 ])
 
 
+ik_inputs = [Input(input_id, 'value') for input_id in IK_INPUT_IDs]
 @app.callback(
-  Output('measurement-display', 'children'),
-  [Input('measurement-dropdown', 'value')])
-def display_measurement(key):
-  try:
-    value = HEXAPOD_MEASUREMENTS[key]
-    return dcc.Markdown('### {} : {}'.format(key, value))
-  except KeyError:
-    return "Nothing"
+  Output('ik-variables', 'children'),
+  ik_inputs)
+def display_variables(
+  start_hip_stance,
+  start_cog_z,
+  end_x,
+  end_y,
+  end_z,
+  rot_x,
+  rot_y,
+  rot_z):
+
+  return dcc.Markdown(f'''
+  ```
+x: {end_x} | rot x: {rot_x}
+y: {end_y} | rot y: {rot_y}
+z: {end_z} | rot z: {rot_z}
+stance: {start_hip_stance} | init z: {start_cog_z}
+  ```
+  '''
+  )
---
+++ widgets/ik_ui.py
@@ -0,0 +1,72 @@
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_daq
+
+IK_INPUT_IDs = [
+  'input-start-stance',
+  'input-start-z',
+  'input-end-x',
+  'input-end-y',
+  'input-end-z',
+  'input-end-rot-x',
+  'input-end-rot-y',
+  'input-end-rot-z',
+]
+
+def make_translate_slider(name, slider_label, slider_size=120):
+  return dash_daq.Slider( # pylint: disable=not-callable
+    id=name,
+    min=-1.0,
+    max=1.0,
+    value=0.05,
+    size=slider_size,
+    updatemode='drag',
+    vertical=True,
+    handleLabel={"showCurrentValue": True,"label": slider_label},
+    step=0.05,
+  )
+
+def make_rotate_knob(name, knob_label):
+  return dash_daq.Knob( # pylint: disable=not-callable
+    id=name,
+    min=-65,
+    max=65,
+    value=0,
+    size=75,
+    scale = {'custom': {0: knob_label, -60: '-60', -30: '-30',  30: '30', 60: '60'}}
+  )
+
+def make_stance_slider(name, slider_label):
+  return dash_daq.Slider( # pylint: disable=not-callable
+    id=name,
+    min=0.0,
+    max=45.0,
+    value=0.0,
+    size=50,
+    updatemode='drag',
+    vertical=True,
+    handleLabel={"showCurrentValue": True,"label": slider_label},
+    step=5.0,
+  )
+
+div_ss = make_stance_slider('input-start-stance', 'start.stance')
+div_sz = make_translate_slider('input-start-z', 'start.z', slider_size=50)
+div_ex = make_translate_slider('input-end-x', 'end.x')
+div_ey = make_translate_slider('input-end-y', 'end.y')
+div_ez = make_translate_slider('input-end-z', 'end.z')
+div_rx = make_rotate_knob('input-end-rot-x', 'rot.x')
+div_ry = make_rotate_knob('input-end-rot-y', 'rot.y')
+div_rz = make_rotate_knob('input-end-rot-z', 'rot.z')
+
+start_section_ik = html.Div([div_ss, html.Br(), div_sz],)
+SECTION_IK = html.Div([
+    html.Div(start_section_ik, style={'padding': '0 0 0 4em'}),
+    html.Div(div_rx),
+    html.Div(div_ex, style={'padding': '0 0 0 3em'}),
+    html.Div(div_ry),
+    html.Div(div_ey, style={'padding': '0 0 0 3em'}),
+    html.Div(div_rz),
+    html.Div(div_ez, style={'padding': '0 0 0 3em'}),
+  ],
+  style={'display': 'flex', 'flex-direction': 'row'}
+)
