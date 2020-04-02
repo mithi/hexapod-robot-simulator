beb9321b1fef6a9c61eb47d2462676ab3b73141b
Mithi <mithi.sevilla@gmail.com>
2020-04-03 01:37:24 +0800

    Change algorithm when can't reach

--- hexapod/ik_solver.py
+++ hexapod/ik_solver.py
@@ -96,29 +96,39 @@ def inverse_kinematics_update(
     dd = angle_opposite_of_last_side(a, b, d)
     ee = angle_between(coxia_vector2d, coxia_to_foot_vector2d)
 
+    alpha_wrt_world = angle_between(coxia_vector, x_axis)
+    alpha = alpha_wrt_world - hexapod.body.COXIA_AXES[i]
+
+    CANT_REACH_FOOT_TIP = False
     if is_triangle_or_line(a, b, d):
       beta = -(180 - aa - ee)
       gamma = dd - 90
     else:
       print("Can't reach foot tip")
+      CANT_REACH_FOOT_TIP = True
       beta = 0
       gamma = 90
 
-    alpha_wrt_world = angle_between(coxia_vector, x_axis)
-    alpha = alpha_wrt_world - hexapod.body.COXIA_AXES[i]
     poses[i]['coxia'] = alpha
     poses[i]['femur'] = beta
     poses[i]['tibia'] = gamma
 
-    height = -p3.z
-    x_ = b * np.cos(np.radians(beta))
-    z_ = b * np.sin(np.radians(beta))
-    x_ = p1.x + x_
-    if height > a:
-      z_ =  -z_ #case 1
+    if CANT_REACH_FOOT_TIP:
+      p2 = deepcopy(p0)
+      p3 = deepcopy(p0)
+      p2.move_xyz(hexapod.femur, 0, 0)
+      p3.move_xyz(hexapod.femur + hexapod.tibia, 0, 0)
+    else:
+      height = -p3.z
+      x_ = b * np.cos(np.radians(beta))
+      z_ = b * np.sin(np.radians(beta))
+      x_ = p1.x + x_
+      if height > a:
+        z_ =  -z_ #case 1
+
+      p2 = Point(x_, 0, z_)
 
     frame = frame_to_align_vector_a_to_b(x_axis, unit_coxia_vector)
-    p2 = Point(x_, 0, z_)
     p0.update_point_wrt(frame)
     p1.update_point_wrt(frame)
     p2.update_point_wrt(frame)
