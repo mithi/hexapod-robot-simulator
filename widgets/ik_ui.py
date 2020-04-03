340bdaf6e4aeeaf62ff801e2275cad825192635e
Mithi <mithi.sevilla@gmail.com>
2020-04-04 00:45:29 +0800

    Convert to vertical slider (not knob)
    - Better user experience

--- widgets/ik_ui.py
+++ widgets/ik_ui.py
@@ -50,6 +50,19 @@ def make_rotate_knob(name, knob_label):
     scale = {'custom': {0: knob_label, -60: '-60', -30: '-30',  30: '30', 60: '60'}}
   )
 
+def make_rotate_slider(name, slider_label):
+  return dash_daq.Slider( # pylint: disable=not-callable
+    id=name,
+    min=-65,
+    max=65,
+    value=0.05,
+    size=120,
+    updatemode='drag',
+    vertical=True,
+    handleLabel={"showCurrentValue": True,"label": slider_label},
+    step=0.05,
+  )
+
 def make_stance_slider(name, slider_label):
   return dash_daq.Slider( # pylint: disable=not-callable
     id=name,
@@ -68,19 +81,19 @@ div_sz = make_h_slider('input-start-leg-stance', 'leg.stance')
 div_ex = make_translate_slider('input-end-x', 'end.x')
 div_ey = make_translate_slider('input-end-y', 'end.y')
 div_ez = make_translate_slider('input-end-z', 'end.z')
-div_rx = make_rotate_knob('input-end-rot-x', 'rot.x')
-div_ry = make_rotate_knob('input-end-rot-y', 'rot.y')
-div_rz = make_rotate_knob('input-end-rot-z', 'rot.z')
+div_rx = make_rotate_slider('input-end-rot-x', 'rot.x')
+div_ry = make_rotate_slider('input-end-rot-y', 'rot.y')
+div_rz = make_rotate_slider('input-end-rot-z', 'rot.z')
 
 start_section_ik = html.Div([div_ss, html.Br(), div_sz],)
 SECTION_IK = html.Div([
     html.Div(start_section_ik, style={'padding': '0 0 0 4em'}),
-    html.Div(div_rx),
-    html.Div(div_ex, style={'padding': '0 0 0 2em'}),
-    html.Div(div_ry),
-    html.Div(div_ey, style={'padding': '0 0 0 2em'}),
-    html.Div(div_rz),
-    html.Div(div_ez, style={'padding': '0 0 0 2em'}),
+    html.Div(div_rx, style={'padding': '0 0 0 4em'}),
+    html.Div(div_ex, style={'padding': '0 0 0 4em'}),
+    html.Div(div_ry, style={'padding': '0 0 0 4em'}),
+    html.Div(div_ey, style={'padding': '0 0 0 4em'}),
+    html.Div(div_rz, style={'padding': '0 0 0 4em'}),
+    html.Div(div_ez, style={'padding': '0 0 0 4em'}),
   ],
   style={'display': 'flex', 'flex-direction': 'row'}
 )
