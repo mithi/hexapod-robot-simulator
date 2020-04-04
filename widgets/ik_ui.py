01d5efb1cc4c8c3f74e38f244045d13af9b6f0f9
Mithi <mithi.sevilla@gmail.com>
2020-04-04 20:13:34 +0800

    User vertical sliders for all variables
    - The knobs are hard to use

--- widgets/ik_ui.py
+++ widgets/ik_ui.py
@@ -14,7 +14,7 @@ IK_INPUT_IDs = [
   'input-end-rot-z',
 ]
 
-def make_translate_slider(name, slider_label, slider_size=120):
+def make_translate_slider(name, slider_label, slider_size=200):
   return dash_daq.Slider( # pylint: disable=not-callable
     id=name,
     min=-1.0,
@@ -27,26 +27,13 @@ def make_translate_slider(name, slider_label, slider_size=120):
     step=0.05,
   )
 
-def make_h_slider(name, slider_label):
-  return dash_daq.Slider( # pylint: disable=not-callable
-    id=name,
-    min=-90,
-    max=90,
-    value=10,
-    size=50,
-    updatemode='drag',
-    vertical=True,
-    handleLabel={"showCurrentValue": True,"label": slider_label},
-    step=0.05,
-  )
-
 def make_rotate_knob(name, knob_label):
   return dash_daq.Knob( # pylint: disable=not-callable
     id=name,
     min=-65,
     max=65,
     value=0,
-    size=65,
+    size=130,
     scale = {'custom': {0: knob_label, -60: '-60', -30: '-30',  30: '30', 60: '60'}}
   )
 
@@ -55,12 +42,25 @@ def make_rotate_slider(name, slider_label):
     id=name,
     min=-65,
     max=65,
-    value=0.05,
-    size=120,
+    value=0.5,
+    size=200,
     updatemode='drag',
     vertical=True,
     handleLabel={"showCurrentValue": True,"label": slider_label},
-    step=0.05,
+    step=0.5,
+  )
+
+def make_h_slider(name, slider_label):
+  return dash_daq.Slider( # pylint: disable=not-callable
+    id=name,
+    min=-90,
+    max=90,
+    value=10,
+    size=80,
+    updatemode='drag',
+    vertical=True,
+    handleLabel={"showCurrentValue": True,"label": slider_label},
+    step=2.0,
   )
 
 def make_stance_slider(name, slider_label):
@@ -69,11 +69,11 @@ def make_stance_slider(name, slider_label):
     min=0.0,
     max=45.0,
     value=0.0,
-    size=50,
+    size=80,
     updatemode='drag',
     vertical=True,
     handleLabel={"showCurrentValue": True,"label": slider_label},
-    step=5.0,
+    step=2.0,
   )
 
 div_ss = make_stance_slider('input-start-hip-stance', 'hip.stance')
@@ -85,15 +85,16 @@ div_rx = make_rotate_slider('input-end-rot-x', 'rot.x')
 div_ry = make_rotate_slider('input-end-rot-y', 'rot.y')
 div_rz = make_rotate_slider('input-end-rot-z', 'rot.z')
 
-start_section_ik = html.Div([div_ss, html.Br(), div_sz],)
+section_ik_start = html.Div([div_ss, html.Br(), div_sz])
+
 SECTION_IK = html.Div([
-    html.Div(start_section_ik, style={'padding': '0 0 0 4em'}),
-    html.Div(div_rx, style={'padding': '0 0 0 4em'}),
+    html.Div(section_ik_start, style={'padding': '0 0 0 4em'}),
     html.Div(div_ex, style={'padding': '0 0 0 4em'}),
-    html.Div(div_ry, style={'padding': '0 0 0 4em'}),
     html.Div(div_ey, style={'padding': '0 0 0 4em'}),
-    html.Div(div_rz, style={'padding': '0 0 0 4em'}),
     html.Div(div_ez, style={'padding': '0 0 0 4em'}),
+    html.Div(div_rx, style={'padding': '0 0 0 4em'}),
+    html.Div(div_ry, style={'padding': '0 0 0 4em'}),
+    html.Div(div_rz, style={'padding': '0 0 0 4em'}),
   ],
   style={'display': 'flex', 'flex-direction': 'row'}
 )
