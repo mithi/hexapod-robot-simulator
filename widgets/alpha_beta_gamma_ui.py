1b89588d4994f8c0147372c4b1497159e30086a1
Mithi <mithi.sevilla@gmail.com>
2020-04-06 01:10:27 +0800

    Use global settings

--- widgets/alpha_beta_gamma_ui.py
+++ widgets/alpha_beta_gamma_ui.py
@@ -2,14 +2,27 @@
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input
+from hexapod.const import ALPHA_MAX_ANGLE, BETA_MAX_ANGLE, GAMMA_MAX_ANGLE
+from widgets.const import UPDATE_MODE
 
 SLIDERS_TEST_IDs = ['slider-alpha', 'slider-beta', 'slider-gamma']
 SLIDERS_TEST_INPUTS = [Input(i, 'value') for i in SLIDERS_TEST_IDs]
 
-SLIDER_ANGLE_MARKS = {tick: str(tick) for tick in [-90, -45, 0, 45, 90]}
-SLIDER_ALPHA = dcc.Slider(id='slider-alpha', min=-90, max=90, marks=SLIDER_ANGLE_MARKS, value=0, step=5, updatemode='drag')
-SLIDER_BETA = dcc.Slider(id='slider-beta', min=-90, max=90, marks=SLIDER_ANGLE_MARKS, value=0, step=5, updatemode='drag')
-SLIDER_GAMMA = dcc.Slider(id='slider-gamma', min=-90, max=90, marks=SLIDER_ANGLE_MARKS, value=0, step=5, updatemode='drag')
+SLIDER_ANGLE_MARKS = {tick: str(tick) for tick in [-45, 0, 45]}
+def make_slider(slider_name, angle_range):
+  return  dcc.Slider(
+    id=slider_name,
+    value=0,
+    min=-angle_range,
+    max=angle_range,
+    step=5,
+    marks=SLIDER_ANGLE_MARKS,
+    updatemode=UPDATE_MODE
+  )
+
+SLIDER_ALPHA = make_slider('slider-alpha', ALPHA_MAX_ANGLE)
+SLIDER_BETA = make_slider('slider-beta', BETA_MAX_ANGLE)
+SLIDER_GAMMA = make_slider('slider-gamma', GAMMA_MAX_ANGLE)
 
 SECTION_SLIDERS_TEST = html.Div([
   html.Div([html.Label('ALPHA'), SLIDER_ALPHA], style={'width': '33%'}),
--- widgets/joint_input_maker.py
+++ widgets/joint_input_maker.py
@@ -1,12 +1,17 @@
-# This module is used to build the widgets
-# for changing the joint angles
+# This module is used to build the widgets for changing the joint angles
 # see also: widgets.pose_control directory
 
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_daq
-from hexapod.const import NAMES_JOINT, NAMES_LEG
+from hexapod.const import NAMES_JOINT, NAMES_LEG, ALPHA_MAX_ANGLE, BETA_MAX_ANGLE, GAMMA_MAX_ANGLE
+from widgets.const import UPDATE_MODE
 
+max_angles = {
+  'coxia': ALPHA_MAX_ANGLE,
+  'femur': BETA_MAX_ANGLE,
+  'tibia': GAMMA_MAX_ANGLE
+}
 # input id format:
 # 'input' + '-' + ['left', 'right'] + '-' + ['front', 'middle', 'back'] + '-' ['coxia', 'femur', 'tibia']
 # input dictionary structure
@@ -20,49 +25,49 @@ def make_all_joint_inputs(joint_input_function):
 
     for joint_name in NAMES_JOINT:
       input_name = 'input-{}-{}'.format(leg_name, joint_name)
-      leg_joint_inputs[joint_name] = joint_input_function(input_name)
+      leg_joint_inputs[joint_name] = joint_input_function(input_name, max_angles[joint_name])
 
     all_joint_inputs[leg_name] = leg_joint_inputs
 
   return all_joint_inputs
 
 
-def make_joint_daq_slider_input(name):
+def make_joint_daq_slider_input(name, max_angle):
   _, _, _, angle = name.split('-')
 
   return dash_daq.Slider( # pylint: disable=not-callable
     id=name,
-    min=-105,
-    max=105,
+    min=-max_angle,
+    max=max_angle,
     value=10,
     size=150,
-    updatemode='drag',
+    updatemode=UPDATE_MODE,
     handleLabel={"showCurrentValue": True,"label": angle},
     step=5,
   )
 
 
-def make_joint_slider_input(name):
+def make_joint_slider_input(name, max_angle):
   slider_marks = {tick: str(tick) for tick in [-90, -45, 0, 45, 90]}
-  return dcc.Slider(id=name, min=-105, max=105, marks=slider_marks, value=0, step=5)
+  return dcc.Slider(id=name, min=-max_angle, max=max_angle, marks=slider_marks, value=0, step=5)
 
 
-def make_joint_number_input(_name):
+def make_joint_number_input(name, max_angle):
   return dcc.Input(
-    id=_name,
+    id=name,
     type='number',
     value=0.0,
     step=5.0,
-    min=-135.0,
-    max=135.0,
+    min=-max_angle,
+    max=max_angle,
     style={'marginRight': '5%', 'width': '95%', 'marginBottom': '5%'})
 
 
-def make_joint_knob_input(name):
+def make_joint_knob_input(name, max_angle):
   return dash_daq.Knob( # pylint: disable=not-callable
     id=name,
-    min=-105,
-    max=100,
+    min=-max_angle,
+    max=max_angle,
     value=0,
     size=75,
     scale = {'custom': {0: '|', -90: '.', 90: '.',  -45: '.', 45: '.'}}
