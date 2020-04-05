526cbfa232b3b7d92f843b6c91688bdde4e23d84
Mithi <mithi.sevilla@gmail.com>
2020-04-05 23:09:51 +0800

    Set update mode to drag

--- widgets/alpha_beta_gamma_ui.py
+++ widgets/alpha_beta_gamma_ui.py
@@ -7,9 +7,9 @@ SLIDERS_TEST_IDs = ['slider-alpha', 'slider-beta', 'slider-gamma']
 SLIDERS_TEST_INPUTS = [Input(i, 'value') for i in SLIDERS_TEST_IDs]
 
 SLIDER_ANGLE_MARKS = {tick: str(tick) for tick in [-90, -45, 0, 45, 90]}
-SLIDER_ALPHA = dcc.Slider(id='slider-alpha', min=-90, max=90, marks=SLIDER_ANGLE_MARKS, value=0, step=5)
-SLIDER_BETA = dcc.Slider(id='slider-beta', min=-90, max=90, marks=SLIDER_ANGLE_MARKS, value=0, step=5)
-SLIDER_GAMMA = dcc.Slider(id='slider-gamma', min=-90, max=90, marks=SLIDER_ANGLE_MARKS, value=0, step=5)
+SLIDER_ALPHA = dcc.Slider(id='slider-alpha', min=-90, max=90, marks=SLIDER_ANGLE_MARKS, value=0, step=5, updatemode='drag')
+SLIDER_BETA = dcc.Slider(id='slider-beta', min=-90, max=90, marks=SLIDER_ANGLE_MARKS, value=0, step=5, updatemode='drag')
+SLIDER_GAMMA = dcc.Slider(id='slider-gamma', min=-90, max=90, marks=SLIDER_ANGLE_MARKS, value=0, step=5, updatemode='drag')
 
 SECTION_SLIDERS_TEST = html.Div([
   html.Div([html.Label('ALPHA'), SLIDER_ALPHA], style={'width': '33%'}),
