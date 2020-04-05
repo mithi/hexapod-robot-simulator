4becb64919f348f83d5834a7cb097289591adc2a
Mithi <mithi.sevilla@gmail.com>
2020-04-05 18:23:29 +0800

    Rearrange functions for better readability

--- hexapod/ground_contact_calculator.py
+++ hexapod/ground_contact_calculator.py
@@ -6,40 +6,42 @@
 # the hexapod's neutral position
 # 3. Determining the height of the center of gravity wrt to the ground
 #
-
-from itertools import combinations
 import numpy as np
+from itertools import combinations
+from .points import Point, dot, get_unit_normal, is_point_inside_triangle
 
-from .points import Point, cross, vector_from_to, scale, dot, length
-from .points import add_vectors, subtract_vectors, get_unit_normal
-from .points import is_point_inside_triangle
+def get_legs_on_ground(legs):
 
-def set_of_two_trios_from_six():
-  # Get all combinations of a three-item-group given six items
-  # 20 combinations total
-  # (2, 3, 5) is a trio from set [0, 1, 2, 3, 4, 5]
-  # the corresponding other_trio of (2, 3, 5) is (0, 1, 4)
-  # order is not important ie (2, 3, 5) is the same as (5, 3, 2)
-  trios = [trio for trio in combinations(range(6), 3)]
-  other_trios = []
+  def within_thresh(a, b, tol=2):
+    return np.abs(a - b) < tol
 
-  for trio in trios:
-    other_trio = [i for i in filter(lambda x: x not in trio, range(6))]
-    other_trios.append(other_trio)
+  trio = three_ids_of_ground_contacts(legs)
 
-  return trios, other_trios
+  # This pose is unstable, The hexapod has no balance
+  if trio is None:
+    return [], None, None
 
-def check_stability(a, b, c):
-  # If the center of gravity p (0, 0) on xy plane
-  # is inside projection (in the xy plane) of
-  # the triangle defined by point a, b, c, then this is stable
-  p = Point(0, 0, 0)
-  return is_point_inside_triangle(p, a, b, c)
+  p0, p1, p2 = get_corresponding_ground_contacts(trio, legs)
+  n = get_unit_normal(p0, p1, p2)
 
-def get_corresponding_ground_contacts(ids, legs):
-  i, j, k = ids
-  return legs[i].ground_contact(), legs[j].ground_contact(), legs[k].ground_contact()
+  # Note: using p0, p1 or p2 should yield the same result
+  cog_from_ground= -dot(n, p0)
 
+  legs_on_ground = []
+
+  # Get all contacts of the same height
+  for leg in legs:
+    ground_contact = -dot(n, leg.ground_contact())
+    if within_thresh(ground_contact, cog_from_ground):
+      legs_on_ground.append(leg)
+
+  return legs_on_ground, n, cog_from_ground
+
+
+# This function takes the legs of the hexapod
+# and finds three legs on the ground that form a stable position
+# returns the leg ids of those three legs
+# return None if no stable position found
 def three_ids_of_ground_contacts(legs):
   trios, other_trios = set_of_two_trios_from_six()
 
@@ -91,29 +93,31 @@ def three_ids_of_ground_contacts(legs):
   # Nothing met the condition
   return None
 
-def get_legs_on_ground(legs):
+# Given the three leg ids and the list of legs
+# get the points contacting the ground of those three legs
+def get_corresponding_ground_contacts(ids, legs):
+  i, j, k = ids
+  return legs[i].ground_contact(), legs[j].ground_contact(), legs[k].ground_contact()
 
-  def within_thresh(a, b, tol=2):
-    return np.abs(a - b) < tol
+# Get all combinations of a three-item-group given six items
+# 20 combinations total
+# (2, 3, 5) is a trio from set [0, 1, 2, 3, 4, 5]
+# the corresponding other_trio of (2, 3, 5) is (0, 1, 4)
+# order is not important ie (2, 3, 5) is the same as (5, 3, 2)
+def set_of_two_trios_from_six():
+  trios = [trio for trio in combinations(range(6), 3)]
+  other_trios = []
 
-  trio = three_ids_of_ground_contacts(legs)
+  for trio in trios:
+    other_trio = [i for i in filter(lambda x: x not in trio, range(6))]
+    other_trios.append(other_trio)
 
-  # This pose is unstable, The hexapod has no balance
-  if trio is None:
-    return [], None, None
+  return trios, other_trios
 
-  p0, p1, p2 = get_corresponding_ground_contacts(trio, legs)
-  n = get_unit_normal(p0, p1, p2)
 
-  # Note: using p0, p1 or p2 should yield the same result
-  cog_from_ground= -dot(n, p0)
-
-  legs_on_ground = []
-
-  # Get all contacts of the same height
-  for leg in legs:
-    ground_contact = -dot(n, leg.ground_contact())
-    if within_thresh(ground_contact, cog_from_ground):
-      legs_on_ground.append(leg)
-
-  return legs_on_ground, n, cog_from_ground
+# If the center of gravity p (0, 0) on xy plane
+# is inside projection (in the xy plane) of
+# the triangle defined by point a, b, c, then this is stable
+def check_stability(a, b, c):
+  p = Point(0, 0, 0)
+  return is_point_inside_triangle(p, a, b, c)
