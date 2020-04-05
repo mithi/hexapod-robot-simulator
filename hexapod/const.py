fb8ef99ef3ff85bcd48b5497dbaa43f98c52e5d6
Mithi <mithi.sevilla@gmail.com>
2020-04-06 01:08:32 +0800

    Add other angle variables

--- hexapod/const.py
+++ hexapod/const.py
@@ -3,7 +3,14 @@ from .models import VirtualHexapod, Hexagon, Linkage
 from .templates.figure_template import HEXAPOD_FIGURE
 from .templates.pose_template import HEXAPOD_POSE
 from .templates.pose_template import PREDEFINED_POSES
-from .MAX_ANGLES import ALPHA_MAX_ANGLE, BETA_MAX_ANGLE, GAMMA_MAX_ANGLE
+from .MAX_ANGLES import (
+  ALPHA_MAX_ANGLE,
+  BETA_MAX_ANGLE,
+  GAMMA_MAX_ANGLE,
+  BODY_MAX_ANGLE,
+  HIP_STANCE_MAX_ANGLE,
+  LEG_STANCE_MAX_ANGLE
+)
 
 # The leg names in leg names start with right middle leg and ends with right back leg (x0 to x5)
 #         x2          x1
