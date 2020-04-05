7be3aa914e704b99c0fd45b60d0ba3ea5ddb8950
Mithi <mithi.sevilla@gmail.com>
2020-04-05 20:11:34 +0800

    Rearrange lines

--- widgets/alpha_beta_gamma.py
+++ widgets/alpha_beta_gamma.py
@@ -3,6 +3,8 @@ import dash_html_components as html
 from dash.dependencies import Input
 
 SLIDERS_TEST_IDs = ['slider-alpha', 'slider-beta', 'slider-gamma']
+SLIDERS_TEST_INPUTS = [Input(i, 'value') for i in SLIDERS_TEST_IDs]
+
 SLIDER_ANGLE_MARKS = {tick: str(tick) for tick in [-90, -45, 0, 45, 90]}
 SLIDER_ALPHA = dcc.Slider(id='slider-alpha', min=-90, max=90, marks=SLIDER_ANGLE_MARKS, value=0, step=5)
 SLIDER_BETA = dcc.Slider(id='slider-beta', min=-90, max=90, marks=SLIDER_ANGLE_MARKS, value=0, step=5)
@@ -15,5 +17,3 @@ SECTION_SLIDERS_TEST = html.Div([
   ],
   style={'display': 'flex'}
 )
-
-SLIDERS_TEST_INPUTS = [Input(i, 'value') for i in SLIDERS_TEST_IDs]
