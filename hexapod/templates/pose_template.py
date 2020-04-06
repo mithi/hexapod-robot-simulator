91f4a46d06648d8ef8c924366954759b145fa3d4
Mithi <mithi.sevilla@gmail.com>
2020-04-07 03:37:30 +0800

    Keep this modules in case we want to bring this feature back

--- hexapod/templates/pose_template.py
+++ hexapod/templates/pose_template.py
@@ -21,47 +21,17 @@ for i, name in enumerate(names):
     'tibia': 0,
   }
 
-pose_tilted = {
-  0: {"coxia": 0, "femur": 5, "tibia": -1, "name": "right-middle", "id": 0},
-  1: {"coxia": -18, "femur": 10, "tibia": -2, "name": "right-front", "id": 1},
-  2: {"coxia": 28, "femur": 44, "tibia": -43, "name": "left-front", "id": 2},
-  3: {"coxia": 0, "femur": 54, "tibia": -43, "name": "left-middle", "id": 3},
-  4: {"coxia": -18, "femur": 45, "tibia": -41, "name": "left-back", "id": 4},
-  5: {"coxia": 28, "femur": 10, "tibia": 1, "name": "right-back", "id": 5}
-}
-
-pose_some_feet_lifted_up  = {
-  0: {"coxia": 0, "femur": 5, "tibia": -1, "name": "right-middle", "id": 0},
-  1: {"coxia": -18, "femur": 40, "tibia": -30, "name": "right-front", "id": 1},
-  2: {"coxia": 28, "femur": 44, "tibia": -43, "name": "left-front", "id": 2},
-  3: {"coxia": 0, "femur": 80, "tibia": -43, "name": "left-middle", "id": 3},
-  4: {"coxia": -18, "femur": 45, "tibia": -41, "name": "left-back", "id": 4},
-  5: {"coxia": 28, "femur": 40, "tibia": 1, "name": "right-back", "id": 5}
-}
-
-pose_twisted_lifted_feet = {
-  0: {"coxia": 0, "femur": 5, "tibia": -1, "name": "right-middle", "id": 0},
-  1: {"coxia": 45, "femur": 40, "tibia": -30, "name": "right-front", "id": 1},
-  2: {"coxia": 28, "femur": 44, "tibia": -43, "name": "left-front", "id": 2},
-  3: {"coxia": 45, "femur": 80, "tibia": -43, "name": "left-middle", "id": 3},
-  4: {"coxia": -18, "femur": 45, "tibia": -41, "name": "left-back", "id": 4},
-  5: {"coxia": 45, "femur": 40, "tibia": 1, "name": "right-back", "id": 5}
-}
-
-pose_twisted_ground = {
-  0: {"coxia": -45, "femur": 5, "tibia": -1, "name": "right-middle", "id": 0},
-  1: {"coxia": -18, "femur": 40, "tibia": -30, "name": "right-front", "id": 1},
-  2: {"coxia": -45, "femur": 44, "tibia": -43, "name": "left-front", "id": 2},
-  3: {"coxia": 0, "femur": 80, "tibia": -43, "name": "left-middle", "id": 3},
-  4: {"coxia": -45, "femur": 45, "tibia": -41, "name": "left-back", "id": 4},
-  5: {"coxia": 28, "femur": 40, "tibia": 1, "name": "right-back", "id": 5}
+example_pose = {
+  0: {"coxia": 16.61, "femur": 28.93 , "tibia": -33.95, "name": "right-middle", "id": 0},
+  1: {"coxia": 23.46, "femur": 44.83, "tibia": -46.41, "name": "right-front", "id": 1},
+  2: {"coxia": 29.53, "femur": 44.86, "tibia": -44.22, "name": "left-front", "id": 2},
+  3: {"coxia": 28.19, "femur": 29.14, "tibia": -31.13 , "name": "left-middle", "id": 3},
+  4: {"coxia": 29.73, "femur": 15.62, "tibia": -13.95, "name": "left-back", "id": 4},
+  5: {"coxia": 20.00, "femur": 15.62, "tibia": -17.44 , "name": "right-back", "id": 5}
 }
 
 PREDEFINED_POSES = {
   'NONE': None,
   'neutral': HEXAPOD_POSE,
-  'pose-tilted': pose_tilted,
-  'pose-lifted-up': pose_some_feet_lifted_up,
-  'pose-twist-lifted':pose_twisted_lifted_feet,
-  'pose-twist-ground':  pose_twisted_ground,
+  'example-pose': example_pose,
 }
