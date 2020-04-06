9921522070ebcfd3564306ad486963ea14efaa3d
Mithi <mithi.sevilla@gmail.com>
2020-04-07 04:01:19 +0800

    Set debug mode to false

--- index.py
+++ index.py
@@ -59,7 +59,7 @@ def display_page(pathname):
 # --------------
 if __name__ == '__main__':
   app.run_server(
-    debug=True,
-    dev_tools_ui=True,
-    dev_tools_props_check=True
+    debug=False,
+    dev_tools_ui=False,
+    dev_tools_props_check=False
   )
