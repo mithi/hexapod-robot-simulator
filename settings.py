f6d9536dc48c604099c5e2d07bf852ee597df6aa
Mithi <mithi.sevilla@gmail.com>
2020-04-12 17:59:10 +0800

    Remove unused requirements, set debug mode to false

--- settings.py
+++ settings.py
@@ -30,7 +30,7 @@ HIP_STANCE_MAX_ANGLE = 45
 # This will make widgets only start updating when you release the mouse button
 UPDATE_MODE = "drag"
 
-DEBUG_MODE = False
+DEBUG_MODE = True
 # The inverse kinematics solver already updates the points of the hexapod
 # but if you want to test whether the pose is indeed correct
 # ie use the poses returned by the inverse kinematics solver
