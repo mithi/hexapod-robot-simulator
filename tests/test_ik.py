2a4021f4714c447d597e608fe32594b4b7ae35b5
Mithi <mithi.sevilla@gmail.com>
2020-04-15 20:20:24 +0800

    Remove blank line

--- tests/test_ik.py
+++ tests/test_ik.py
@@ -6,17 +6,12 @@ from tests.ik_cases.case1 import given_dimensions, given_ik_parameters, correct_
 from tests.helpers import assert_poses_equal
 
 
+def assert_ik_solver(ik_function):
+    hexapod = VirtualHexapod(given_dimensions)
+    result_poses, hexapod = ik_function(hexapod, given_ik_parameters)
+    assert_poses_equal(result_poses, correct_poses)
+
+
 def test_sample_ik():
-    hexapod = VirtualHexapod(given_dimensions)
-    result_poses, hexapod = ik_solver.inverse_kinematics_update(
-        hexapod, given_ik_parameters
-    )
-    assert_poses_equal(result_poses, correct_poses)
-
-
-def test_sample_ik2():
-    hexapod = VirtualHexapod(given_dimensions)
-    result_poses, hexapod = ik_solver2.inverse_kinematics_update(
-        hexapod, given_ik_parameters
-    )
-    assert_poses_equal(result_poses, correct_poses)
+    assert_ik_solver(ik_solver2.inverse_kinematics_update)
+    assert_ik_solver(ik_solver.inverse_kinematics_update)
