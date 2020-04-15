b089e18b6cbde2e2b4de0d0d88229aab9dc1488e
Mithi <mithi.sevilla@gmail.com>
2020-04-15 20:15:36 +0800

    Fix format

--- tests/test_ik.py
+++ tests/test_ik.py
@@ -8,10 +8,15 @@ from tests.helpers import assert_poses_equal
 
 def test_sample_ik():
     hexapod = VirtualHexapod(given_dimensions)
-    result_poses, hexapod = ik_solver.inverse_kinematics_update(hexapod, given_ik_parameters)
+    result_poses, hexapod = ik_solver.inverse_kinematics_update(
+        hexapod, given_ik_parameters
+    )
     assert_poses_equal(result_poses, correct_poses)
+
 
 def test_sample_ik2():
     hexapod = VirtualHexapod(given_dimensions)
-    result_poses, hexapod = ik_solver2.inverse_kinematics_update(hexapod, given_ik_parameters)
+    result_poses, hexapod = ik_solver2.inverse_kinematics_update(
+        hexapod, given_ik_parameters
+    )
     assert_poses_equal(result_poses, correct_poses)
