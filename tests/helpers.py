03d5a3cf0546d5f98bf58eb64fd43b8545e8039f
Mithi <mithi.sevilla@gmail.com>
2020-04-23 19:36:58 +0800

    Add tolerance when asserting points in tests

--- tests/helpers.py
+++ tests/helpers.py
@@ -18,11 +18,11 @@ def assert_hexapod_points_equal(
         return f"Unequal Points\nexpected: {a}\n....found: {b}\n(case: {description})"
 
     for point_a, point_b in zip(correct_body_points, hexapod.body.all_points):
-        assert point_a == point_b, msg(point_a, point_b)
+        assert point_a.__eq__(point_b, percent_tol=0.0075), msg(point_a, point_b)
 
     for leg_set, leg in zip(correct_leg_points, hexapod.legs):
         for point_a, point_b in zip(leg_set, leg.all_points):
-            assert point_a == point_b, msg(point_a, point_b)
+            assert point_a.__eq__(point_b, percent_tol=0.0075), msg(point_a, point_b)
 
 
 def assert_two_hexapods_equal(hexapod1, hexapod2, description):
@@ -30,8 +30,8 @@ def assert_two_hexapods_equal(hexapod1, hexapod2, description):
         return f"Unequal Points\n1: {a}\n2:{b}\n(case: {description})"
 
     for point_a, point_b in zip(hexapod1.body.all_points, hexapod2.body.all_points):
-        assert point_a == point_b, msg(point_a, point_b)
+        assert point_a.__eq__(point_b, percent_tol=0.0075), msg(point_a, point_b)
 
     for leg_a, leg_b in zip(hexapod1.legs, hexapod2.legs):
         for point_a, point_b in zip(leg_a.all_points, leg_b.all_points):
-            assert point_a == point_b, msg(point_a, point_b)
+            assert point_a.__eq__(point_b, percent_tol=0.0075), msg(point_a, point_b)
